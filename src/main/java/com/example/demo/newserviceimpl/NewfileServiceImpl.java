ed95bf6f8d74de9e93e16889be13b007a8532505
bala-amypo <bala@amypo.in>
2025-12-17 09:44:56 +0000

    User commit

--- src/main/java/com/example/demo/newserviceimpl/NewfileServiceImpl.java
+++ src/main/java/com/example/demo/newserviceimpl/NewfileServiceImpl.java
@@ -1,33 +1,38 @@
+import com.example.demo.newservice.NewfileServiceImpl;
 package com.example.demo.newservice.newserviceimpl;
-import java.util.List;
-import org. springframework.stereotype.Service;
-import com.example.demo.newentity. NewfileEntity;
-import com.example.demo.newservice.NewfileService;
-@Service
+
+import com.example.demo.newentity.NewFileEntity;
+import java.util.*;
+
 public class NewfileServiceImpl implements NewfileService{
-    private final NewfileRepo rep;
 
-public NewfileServiceImpl(NewfileRepo rep){
-this.rep=rep;
-}
+    private final NewfileRepo repo;
+    @Override
+    public NewfileServiceImpl(NewfileRepo repo){
+        this.repo = repo;
+    }
 
-@Override
-public NewfileEntity savedata(NewfileEntity newfile){
-return rep.save(newfile);
-}
-@Override
-public NewfileEntity savedata(NewfileEntity newfile) {
-}
-@Override
-public NewfileEntity getidval(Long id) {
-}
-@Override
-public List<NewfileEntity> getall() {
-}
-@Override
-public NewfileEntity update(Long id, NewfileEntity newfile) {
-}
-@Override
-public void delete(Long id) {
-}
+    public NewFileEntity savedata(NewFileEntity st){
+        return repo.save(st);
+    }
+
+    @Override
+    public NewFileEntity getidval(Long id){
+        return findById(id);
+    }
+
+    @Override
+    public List<NewFileEntity> getalldata(){
+        return findAll();
+    }
+
+    @Override
+    public NewFileEntity update(Long id,NewFileEntity st){
+
+    }
+
+    @Override
+    public void del(Long id){
+
+    }
 }
