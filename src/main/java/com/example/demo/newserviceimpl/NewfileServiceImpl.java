be8133a0aa133eae95dea13dee8462874c02ae8b
bala-amypo <bala@amypo.in>
2025-12-17 09:14:56 +0000

    User commit

---
+++ src/main/java/com/example/demo/newserviceimpl/NewfileServiceImpl.java
@@ -0,0 +1,12 @@
+package com.example.java.newserviceimpl;
+
+public class NewfileServiceImpl implements NewfileService{
+
+    @Override
+    public NewfileEntity savedata(NewfileEntity newfile){
+
+    }   
+    @Override 
+
+    
+}
